47a415df1d11d6cfcf62318b917022b01c3bd0eb
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2026-01-06 12:42:24 +0100

    feat(page): add localized home page

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import Home from "@/components/Home";
 
-export default function HomePage() {
-  return <Home />;
+export default function LocaleHomePage() {
+  return ( <Home />);
 }
