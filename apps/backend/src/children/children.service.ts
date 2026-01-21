df4f31fd642767f8bcb223322f93dfce44de9a0a
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2026-01-21 18:18:14 +0100

    remove unused imports

--- apps/backend/src/children/children.service.ts
+++ apps/backend/src/children/children.service.ts
@@ -1,10 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Child } from '../entities/child.entity';
-import { CreateChildDto } from './dto/create-child.dto';
-import { UpdateChildDto } from './dto/update-child.dto';
-
 @Injectable()
 export class ChildrenService {
   constructor(
